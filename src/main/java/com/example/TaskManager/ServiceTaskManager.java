1e5cd979078a471cf20d4075af7962e9daa7bc91
WhiteGeorg <koffinar@student.21-school.ru>
2026-01-28 00:57:59 +0300

    Add Permission check to putTaskById and append new constraint to postNewTask Permission check

--- src/main/java/com/example/TaskManager/ServiceTaskManager.java
+++ src/main/java/com/example/TaskManager/ServiceTaskManager.java
@@ -13,23 +13,24 @@ import java.util.concurrent.atomic.AtomicLong;
 public class ServiceTaskManager {
     HashMap<Long, Task> mapTasks;
     AtomicLong idCounter;
+
     ServiceTaskManager() {
         mapTasks = new HashMap<Long, Task>();
         idCounter = new AtomicLong();
     }
 
     public Task postNewTask(Task task) {
-        if (task.getId() != null)
+        if (task.getId() != null || task.getStatus() != null)
             throw new IllegalArgumentException("Permission denied");
         var newTask = new Task(
                 idCounter.incrementAndGet(),
                 task.getCreatorId(),
                 task.getAssignedId(),
-                task.getStatus(),
+                TaskStatus.CREATED,
                 task.getCreateDateTime(),
                 task.getDeadlineDate(),
                 task.getPriority());
-        mapTasks.put(newTask.getId(),newTask);
+        mapTasks.put(newTask.getId(), newTask);
 
         return newTask;
     }
@@ -53,11 +54,13 @@ public class ServiceTaskManager {
     }
 
     public Task putTaskById(Long id, Task task) {
-
         if (!task.getId().equals(id))
             throw new IllegalArgumentException("ID are Different");
         if (!mapTasks.containsKey(id))
             throw new NoSuchElementException("Can not find any tasks with id " + id);
+        var taskToUpdate = mapTasks.get(id);
+        if (taskToUpdate.getStatus().equals(TaskStatus.DONE))
+            throw new IllegalArgumentException("PERMISSION DENIED task already DONE");
         var newTask = new Task(
                 task.getId(),
                 task.getCreatorId(),
@@ -66,7 +69,8 @@ public class ServiceTaskManager {
                 task.getCreateDateTime(),
                 task.getDeadlineDate(),
                 task.getPriority());
-        mapTasks.put(newTask.getId(),newTask);
+        mapTasks.put(newTask.getId(), newTask);
         return newTask;
     }
+
 }
